386a6c869ac9d1ec8ee510f0af6a847821206fff
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-05-15 14:01:41 +0300

    edit readme and grade

--- Demo.cpp
+++ Demo.cpp
@@ -5,33 +5,6 @@
   @since  2020-05
 **/
 
-/*
-Each player will have 8 soldiers:
-1 soldier commander
-2 soldiers
-1 sniper commander
-1 sniper
-1 paramedic commander
-2 paramedics
-
-The grid wil be set to 14X17
-
-	X	S-C		X	SNPR-C	X	P-C		X
-	S	X		S	SNPR	P	X		P
-	X	X		X	X		X	X		X
-	X	X		X	X		X	X		X
-	X	X		X	X		X	X		X
-	X	X		X	X		X	X		X
-	S	X		S	SNPR	P	X		P
-	X	S-C		X	SNPR-C	X	P-C		X
-
-X is blank
-S is soldier
-S-C soldier commandr
-...
-
-*/
-
 struct Move {
 	int x, y;
 };
