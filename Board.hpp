bf53864aebc46e290e2e20bdeba3ad12a703cbe7
Erel Segal-Halevi cron <erelsgl@gmail.com>
2020-05-18 10:25:06 +0300

    minor edits

--- Board.hpp
+++ Board.hpp
@@ -1,24 +1,31 @@
+/**
+ * Header file for the board of the war game.
+ * 
+ * @author Oz Levi 
+ * @author Erel Segal-Halevi
+ * @since  2020-05
+ */
+
 #include <string>
 #include <vector>
 #include <stdexcept>
 
 namespace WarGame {
-class Board
-{
- private:
-    std::vector<std::vector<void*>> board;
- public:
+
+class Board {
+  private:
+    std::vector<std::vector<Soldier*>> board;
+  public:
     enum MoveDIR { Up, Down, Right, Left };
     
-    Board(uint numRows, uint numCols) : board(numRows, std::vector<void*>(numCols, nullptr)){};
+    Board(uint numRows, uint numCols) : board(numRows, std::vector<Soldier*>(numCols, nullptr)){};
 
     // operator for putting soldiers on the game-board during initialization.
-    void*& operator[](std::pair<int,int> location);
-    
+    Soldier*& operator[](std::pair<int,int> location);
+
     // operator for reading which soldiers are on the game-board.
-    void* operator[](std::pair<int,int> location) const;
-    
-    
+    Soldier* operator[](std::pair<int,int> location) const;
+
     // Try to move the soldier of player "player"
     //     from the "source" location to the "target" location.
     // If the move is illegal, it throws "std::invalid_argument". 
@@ -26,13 +33,12 @@ class Board
     //  * There is no soldier in the source location (i.e., the soldier pointer is null);
     //  * The soldier in the source location belongs to the other player.
     //  * There is already another soldier (of this or the other player) in the target location.
-    //  * The specific soldier at the source location cannot move to the target location
-    //    (it is not a legal move for him - e.g. too far away).
-    std::pair<int,int> move(int player_number, std::pair<int,int> source, MoveDIR dest);
+    std::pair<int,int> move(int player_number, std::pair<int,int> source, MoveDIR direction);
     // You have to implement this function.
     // HINT: Do not write code that depends on the type of soldier!
     // Your code should be generic. All handling of different types of soldiers 
     //      must be handled by polymorphism.
     //throw new std::invalid_argument{};
 };
+
 }
--- Game.hpp
+++ Game.hpp
@@ -2,6 +2,7 @@
 /**
  * Header file for the war game.
  * 
+ * @author Oz Levi 
  * @author Erel Segal-Halevi
  * @since  2020-05
  */
@@ -12,26 +13,26 @@
 namespace WarGame {
 	class Game {
 	 private:
-		Board* borad;
+		Board board;
 	 public:
-	 
+
  		/**
-		 *  Game Cons will initilize board and place soldiers.
+		 *  Game Constructor will initilize board and place soldiers.
 		 */
-		Game(uint numRows, uint numCols){};
+		Game(uint numRows, uint numCols);
 		/*
 		{
 			board(numRows, numCols) 
-			borad[(0,1)] = new FootSoldier(1);
-			borad[(0,3)] = new FootCommander(1);
-			borad[(0,5)] = new FootSoldier(1);
+			board[(0,1)] = new FootSoldier(1);
+			board[(0,3)] = new FootCommander(1);
+			board[(0,5)] = new FootSoldier(1);
 		
-			borad[(13,1)] = new FootSoldier(2);
-			borad[(13,3)] = new FootCommander(2);
-			borad[(13,5)] = new FootSoldier(2);
+			board[(13,1)] = new FootSoldier(2);
+			board[(13,3)] = new FootCommander(2);
+			board[(13,5)] = new FootSoldier(2);
 		}
 		*/
-		
+
 		/**
 		 *  Play the game and return the winner (1 or 2).
 		 */
@@ -42,19 +43,19 @@ namespace WarGame {
 			uint winner = winner(); // should be 0 - there is no winner yet.
 			if (winner) return winner;
 		
-			move_res = borad.move(1, make_pair(1,1), Board::MoveDIR::Up);    // FootSoldier of player 1 moves forward.
+			move_res = board.move(1, make_pair(1,1), Board::MoveDIR::Up);    // FootSoldier of player 1 moves forward.
 			activate(move_res);	// active soldier ability
 			if (winner) return winner;
 			
-			borad.move(2, make_pair(12,1), Board::MoveDIR::Down);  // FootSoldier of player 2 moves forward. if there is an enemy around he attacks.
+			board.move(2, make_pair(12,1), Board::MoveDIR::Down);  // FootSoldier of player 2 moves forward. if there is an enemy around he attacks.
 			activate(move_res);	// active soldier ability
 			if (winner) return winner;
 			
-			borad.move(1,  make_pair(0,2), Board::MoveDIR::Up);    // FootCommander of player 1 moves forward, and all soldiers of player 1 attack.
+			board.move(1,  make_pair(0,2), Board::MoveDIR::Up);    // FootCommander of player 1 moves forward, and all soldiers of player 1 attack.
 			activate(move_res);	// active soldier ability
 			if (winner) return winner;
 			
-			borad.move(2,  make_pair(13,2), Board::MoveDIR::Down);    // FootCommander of player 2 moves forward, and all soldiers of player 2 attack.
+			board.move(2,  make_pair(13,2), Board::MoveDIR::Down);    // FootCommander of player 2 moves forward, and all soldiers of player 2 attack.
 			activate(move_res);	// active soldier ability
 			if (winner) return winner;
 		
@@ -63,19 +64,19 @@ namespace WarGame {
 			return game.winner();
 		}
 		*/
-		
+
 		// operator for reading which soldiers are on the game-board.
 		// will help you check your game moves
 		// will return "FootSoldier" / "Empty" ....
-    	std::string operator[](std::pair<int,int> location) const;
+    	Soldier* operator[](std::pair<int,int> location) const;
 
 		// Check if the game is over, and if so - who is the winner.
 		// returns 0 if the game is not over (i.e., both players have remaining soldiers).
 		// returns 1 if the game is over and player 1 won.
 		// returns 2 if the game is over and player 2 won.
 		uint winner() const ;
-		
-		//activate the soldier placed in the input location abitlity
-		void activate(std::pair<int,int> lovation);
+
+		//activate the special ability of the soldier placed in the given location.
+		void activate(std::pair<int,int> location);
 	};
 }
