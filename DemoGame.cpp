db0b6bed4bb81f77896f03147d92e4b55388379d
Oz <ozmaatuk@gmail.com>
2020-05-25 03:28:34 +0300

    Update DemoGame.cpp
    fixed comments

--- DemoGame.cpp
+++ DemoGame.cpp
@@ -34,16 +34,16 @@ namespace WarGame {
 		}
 
 		uint DemoGame::play() {
-			board.move(1, {0,1}, Board::MoveDIR::Up);      // FootSoldier of player 1 moves forward and attacks from {1,1} to {0,2}.
+			board.move(1, {0,1}, Board::MoveDIR::Up);      // FootSoldier of player 1 moves forward and attacks from {0,1} to {1,1}.
 			if (!board.has_soldiers(2)) return 1;
 
-			board.move(2, {7,1}, Board::MoveDIR::Down);    // FootSoldier of player 2 moves forward and attacks from {6,1} to {7,}.
+			board.move(2, {7,1}, Board::MoveDIR::Down);    // FootSoldier of player 2 moves forward and attacks from {7,1} to {6,1}.
 			if (!board.has_soldiers(1)) return 2;
 
 			board.move(1, {0,3}, Board::MoveDIR::Up);      // FootCommander of player 1 moves forward from {0,3} to {1,3}, and all soldiers of player 1 attack.
 			if (!board.has_soldiers(2)) return 1;
 
-			board.move(2, {7,3}, Board::MoveDIR::Left);    // FootCommander of player 2 moves left from {7,3} to {7,4}, and all soldiers of player 2 attack.
+			board.move(2, {7,3}, Board::MoveDIR::Left);    // FootCommander of player 2 moves left from {7,3} to {7,2}, and all soldiers of player 2 attack.
 			if (!board.has_soldiers(1)) return 2;
 
 			/// Write more moves here..
