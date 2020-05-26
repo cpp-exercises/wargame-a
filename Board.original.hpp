52cb929bf1fbf27105127be7bf9a15903330a5bd
Erel Segal-Halevi <erelsgl@gmail.com>
2020-05-26 18:03:51 +0300

    rename board.hpp

---
+++ Board.original.hpp
@@ -0,0 +1,75 @@
+/**
+ * Header file for the board of the war game.
+ * 
+ * You can copy this file to a new file called Board.hpp, and extend it as you like.
+ * 
+ * @author Oz Levi 
+ * @author Erel Segal-Halevi
+ * @since  2020-05
+ */
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include "Soldier.hpp"
+
+
+namespace WarGame {
+
+struct SoldierProxy {
+  Soldier*& soldier;
+  SoldierProxy(Soldier*& the_soldier): soldier(the_soldier) {}
+
+  operator=(Soldier* new_soldier) {
+    if (soldier==nullptr) {
+      soldier = new_soldier;
+    } else {
+      throw _exception("Cannot override a non-null soldier");
+    }
+  }
+
+  operator Soldier*() {
+    return soldier;
+  }
+};
+
+class Board {
+  private:
+    std::vector<std::vector<Soldier*>> board;
+  public:
+    enum MoveDIR { Up, Down, Right, Left };
+    
+    Board(uint numRows, uint numCols) : 
+      board(numRows, std::vector<Soldier*>(numCols, nullptr)) {}
+
+    // operator for putting soldiers on the game-board during initialization.
+    SoldierProxy operator[](std::pair<int,int> location) {
+        return board[location.first][location.second];
+    }
+    
+    // operator for reading which soldiers are on the game-board.
+    Soldier* operator[](std::pair<int,int> location) const;
+    
+    // The function "move" tries to move the soldier of player "player"
+    //     from the "source" location to the "target" location,
+    //     and activates the special ability of the soldier.
+    // If the move is illegal, it throws "std::invalid_argument". 
+    // Illegal moves include:
+    //  * There is no soldier in the source location (i.e., the soldier pointer is null);
+    //  * The soldier in the source location belongs to the other player.
+    //  * There is already another soldier (of this or the other player) in the target location.
+    // IMPLEMENTATION HINT: Do not write "if" conditions that depend on the type of soldier!
+    // Your code should be generic. All handling of different types of soldiers 
+    //      must be handled by polymorphism.
+    void move(uint player_number, std::pair<int,int> source, MoveDIR direction);
+
+    // returns true iff the board contains one or more soldiers of the given player.
+    bool has_soldiers(uint player_number) const;
+    
+    ~Board()
+    {
+      board.clear();
+    }
+};
+
+}
